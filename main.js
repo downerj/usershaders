19e1157188759db773cdb5ae96e5b813d4d7fa65
James Downer <downer.james.256k@gmail.com>
2022-01-12 23:13:19 -0700

    IntervalTimer & Application

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+class Application {
+  #canvas;
+  #timer = new IntervalTimer(this.#onTick.bind(this), 10);
+  
+  constructor(canvas) {
+    this.#canvas = canvas;
+  }
+  
+  run() {
+    this.#timer.resume();
+  }
+  
+  pause() {
+    this.#timer.suspend();
+  }
+  
+  #onTick(timestamp) {
+    return true;
+  }
+}
+
 window.addEventListener('load', () => {
   const cvs = document.getElementById('cvs');
   
@@ -8,5 +29,6 @@ window.addEventListener('load', () => {
   window.addEventListener('resize', window_onResize);
   window_onResize();
   
-  
+  const app = new Application(cvs);
+  app.run();
 });
