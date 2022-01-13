28321df38222f719546401ee6e26ab519cd2a65a
James Downer <downer.james.256k@gmail.com>
2022-01-13 00:09:43 -0700

    Working rainbow circle program

--- shaders.js
+++ shaders.js
@@ -3,7 +3,7 @@ precision highp float;
 
 attribute vec2 inPosition;
 
-void main() {
+void main(void) {
   gl_Position = vec4(inPosition, 0.0, 1.0);
 }
 `;
@@ -15,10 +15,37 @@ precision highp float;
 precision mediump float;
 #endif
 
+#define PI 3.141592653589793
+
+vec3 hsv2rgb(in vec3 hsv) {
+  float h = hsv.x;
+  float s = hsv.y;
+  float v = hsv.z;
+  vec3 k = vec3(1.0, 2.0/3.0, 1.0/3.0);
+  vec3 p = clamp(abs(6.0 * fract(h - k) - 3.0) - 1.0, 0.0, 1.0);
+  return v * mix(k.xxx, p, s);
+}
+
 uniform vec2 uResolution;
 uniform float uTime;
 
-void main() {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+const float spread = 0.005;
+const float speed = 0.0001;
+
+void setColor(out vec4 fragColor, in vec4 fragCoord) {
+  float w = uResolution.x;
+  float h = uResolution.y;
+  float x = fragCoord.x;
+  float y = fragCoord.y;
+  float xp = x - w*0.5;
+  float yp = y - h*0.5;
+  float value = sqrt(xp*xp + yp*yp);
+  float hue = mod(value * spread + uTime * speed, 360.0);
+  vec3 rgb = hsv2rgb(vec3(hue, 1.0, 1.0));
+  fragColor = vec4(rgb, 1.0);
+}
+
+void main(void) {
+  setColor(gl_FragColor, gl_FragCoord);
 }
 `;
