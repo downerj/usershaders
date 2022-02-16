52a48286bd50726a36e3be3e96929609059a8b25
James Downer <downer.james.256k@gmail.com>
2022-02-16 08:20:10 -0700

    Add Mandelbrot Set fragment

--- shaders.js
+++ shaders.js
@@ -37,6 +37,40 @@ uniform struct {
   float e;
 } user;`;
 
+const mandelbrotFragment = `
+int mandelbrot(in vec2 uv) {
+  vec2 p = vec2(0.0, 0.0);
+  const int maxIterations = 1000;
+  for (int i = 0; i < maxIterations - 1; i++) {
+    if (p.x*p.x + p.y*p.y > 4.0) {
+      return i;
+    }
+    p = vec2(p.x*p.x - p.y*p.y, 2.0*p.x*p.y) + uv;
+  }
+  return -1;
+}
+
+void setColor(out vec4 fragColor, in vec4 fragCoord) {
+  vec2 c = resolution*0.5;
+  float scale = resolution.y;
+  vec2 offset = vec2(-1.0, 0.3);
+  float zoom = 10.0;
+  vec2 uv = (fragCoord.xy - c)/(scale*zoom) + offset;
+
+  int iterations = mandelbrot(uv);
+  if (iterations < 0) {
+    fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    return;
+  }
+
+  float value = float(iterations)/36.0;
+  float speed = 1.0/2500.0;
+  float hue = fract(value + time*speed);
+  vec3 rgb = hsv2rgb(vec3(hue, 1.0, 1.0));
+  fragColor = vec4(rgb, 1.0);
+}
+`.trim();
+
 function makeRainbowFragment(valueSegment) {
   return `
 void setColor(out vec4 fragColor, in vec4 fragCoord) {
@@ -57,6 +91,7 @@ const fragmentsMain = {
   'Diagonal Lines': makeRainbowFragment('float value = p.y - p.x;'),
   'Hyperbolas A': makeRainbowFragment('float value = sqrt(abs(p.x*p.x - p.y*p.y));'),
   'Hyperbolas B': makeRainbowFragment('float value = p.x*p.x/p.y - p.y;'),
+  'Mandelbrot Set': mandelbrotFragment,
   'Parabolas': makeRainbowFragment('float value = p.x*p.x/p.y;'),
 };
 
